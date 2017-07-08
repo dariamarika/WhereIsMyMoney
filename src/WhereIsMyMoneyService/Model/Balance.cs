ab23032cde2c4687db7922024bb38296b2eb590a
shichang <houshichang@hotmail.com>
2017-07-08 23:04:52 +1200

    Added service and repository
    Added service layer and repository layer. Added models

---
+++ src/WhereIsMyMoneyService/Model/Balance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WhereIsMyMoneyService.Model
+{
+    public class Balance
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public float Amount { get; set; }
+    }
+}
---
+++ src/WhereIsMyMoneyService/Model/Wallet.cs
@@ -0,0 +1,10 @@
+﻿namespace WhereIsMyMoneyService.Model
+{
+    public class Wallet
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
